33d962aa28c92b55b95ff2531c23e38b8ff256fd
IAlexassassinI <98475553+IAlexassassinI@users.noreply.github.com>
2025-03-23 17:09:01 +0200

    Update image_generator_config.py to allow use of wfc

--- api/image_generator_config.py
+++ api/image_generator_config.py
@@ -5,7 +5,7 @@ from image_generator.algorithms.hex_pattern_algorithm import HexPatternAlgorithm
 from image_generator.algorithms.plain_algorithm import PlainAlgorithm
 from image_generator.algorithms.smooth_wave_background_algorithm import SmoothWaveBackgroundAlgorithm
 from image_generator.algorithms.voronoi_algorithm import VoronoiAlgorithm
-#from image_generator.algorithms.wfc_algorithm import WFCAlgorithm
+from image_generator.algorithms.wfc_algorithm import WFCAlgorithm
 from image_generator.blendings.alpha_blending import AlphaBlending
 from image_generator.algorithms.flow_field_algorithm import FlowFieldGenerator
 from image_generator.algorithms.noises import ProceduralBackgroundGenerator
@@ -18,7 +18,7 @@ smooth_wave_algorithm = SmoothWaveBackgroundAlgorithm("smooth_wave_background", 
 gradient_algorithm = GradientAlgorithm("gradient_algorithm", "Gradient Algorithm")
 voronoi_algorithm = VoronoiAlgorithm("voronoi_algorithm", "Voronoi Algorithm")
 hex_pattern_algorithm = HexPatternAlgorithm("hex_pattern_algorithm", "Hex Pattern Algorithm")
-#wave_function_collapse_algorithm = WFCAlgorithm("wfc", "Wave Function Collapse")
+wave_function_collapse_algorithm = WFCAlgorithm("wfc", "Wave Function Collapse")
 flow_field_generator = FlowFieldGenerator("flow_field_generator", "Flow Field Generator")
 procedural_background_generator = ProceduralBackgroundGenerator("procedural_background_generator", "Procedural Background Generator")
 image_generator = ImageGenerator(generators=[lorenz_algorithm,
@@ -28,6 +28,6 @@ image_generator = ImageGenerator(generators=[lorenz_algorithm,
                                              gradient_algorithm,
                                              voronoi_algorithm,
                                              hex_pattern_algorithm,
-                                             #wave_function_collapse_algorithm,
+                                             wave_function_collapse_algorithm,
                                              flow_field_generator,
                                              procedural_background_generator])
