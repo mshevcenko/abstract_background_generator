91d469a5a192ed9ee3f48ba25ba861600906f818
IAlexassassinI <alexassassin.shev@gmail.com>
2025-03-27 16:55:32 +0200

    removed debug image.show()

--- image_generator/blendings/zone_blending.py
+++ image_generator/blendings/zone_blending.py
@@ -280,13 +280,15 @@ class ZoneBlendingNamed(Blending):
                  seed: int = None,
                  area: Optional[List[List[bool]]] = None,
                  **kwargs) -> Image:
-        random.seed(seed)
         zones_matrix = generate_zones_matrix(self.zone_gen_fun,
                                              monochrome=True,
                                              width=width,
                                              height=height,
+                                             seed=seed,
+                                             area=area,
                                              **kwargs
                                              )
+        random.seed(seed)
         zones_info = generate_zone_info(zones_matrix, layers)
 
         combined_image = combine_images_using_zones(
